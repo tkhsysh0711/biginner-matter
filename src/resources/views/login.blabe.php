04b6ce242d11420bd7cbf31f40a715f7149e9500
Takahashi <ahg945127@gmail.com>
2024-02-19 18:11:25 +0900

    ログインページの作成

---
+++ src/resources/views/login.blabe.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}" />
+</head>
+<body>
+    <header class="header">
+        <div class="header_inner">
+            <h1 class="header_logo">Atte</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="login_content">
+            
+        </div>
+    </main>
+</body>
+</html>
