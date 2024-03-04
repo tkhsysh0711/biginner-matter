b21d279c4793fe47ad51e807f5b1d89c6477bc6a
Takahashi <ahg945127@gmail.com>
2024-03-04 17:20:58 +0900

    打刻ページの作成

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,7 +11,39 @@
 <body>
     <header class="header">
 
-    </header>
+    <div class="header__logo">
+      <a href="/">Atte</a>
+    </div>
+
+      <nav>
+
+        <ul class="header-nav">
+
+          <li class="header-nav__item">
+            <a class="header-nav__link" href="/">ホーム</a>
+          </li>
+
+          <li class="header-nav__item">
+            <a class="header-nav__link" href="/user">会員一覧</a>
+          </li>
+
+          <li class="header-nav__item">
+            <a class="header-nav__link" href="/attendance">日付一覧</a>
+          </li>
+
+          <li class="header-nav__item">
+            <form class="logout-form" method="post" action="/logout">
+              @csrf
+              <button class="header-nav__button">ログアウト</button>
+            </form>
+          </li>
+
+        </ul>
+
+      </nav>
+
+  </header>
+
 
     <main>
         <div class="main-title">
@@ -26,31 +58,38 @@
 
                 <form class="stamp-form" action="/attendance/start" method="post">
                     @csrf
-                    <button class='menu__button' type="submit" {{ filled($start) ? 'disabled' : '' }}>勤務開始</button>
+                    <button class="stamp-button" type="submit" @if ($status['isWorking']) disabled @endif>勤務開始</button>
                 </form>
 
                 <form class="stamp-form" action="/attendance/end" method="post">
-                    @csrf
-                    @method('patch')
-                    <button class="stamp-button" type="submit" @if (!$status['']) disabled @endif>勤務終了</button>
-                </form>
+                @csrf
+                @method('patch')
+                <button class="stamp-button" type="submit" @if (!$status['isWorking']) disabled @endif>勤務終了</button>
+            </form>
 
-            </div>
+        </div>
 
         <div class="stamp-row">
 
             <form class="stamp-form" action="/rest/start" method="post">
                 @csrf
-                <button class="stamp-button" type="submit" @if (!$status[''] or $status['']) disabled @endif>休憩開始</button>
+                @method('patch')
+                <button class="stamp-button" type="submit" @if (!$status['isWorking'] or $status['isBreaking']) disabled @endif>休憩開始</button>
             </form>
 
             <form class="stamp-form" action="/rest/end" method="post">
                 @csrf
                 @method('patch')
-                <button class="stamp-button" type="submit" @if (!$status[''] or !$status['']) disabled @endif>休憩終了</button>
+                <button class="stamp-button" type="submit" @if (!$status['isWorking'] or !$status['isBreaking']) disabled @endif>休憩終了</button>
             </form>
 
         </div>
-</main>
+    </main>
+
+    <footer class="footer">
+        <div class="footer__inner">
+            <small class="credit">Atte, inc.</small>
+        </div>
+    </footer>
 </body>
 </html>
